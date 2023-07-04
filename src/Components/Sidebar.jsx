4845140b062475e724605e3ca5d524008c80c44f
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 18:42:53 +0200

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -261,7 +261,7 @@ const Sidebar = () => {
         </div>
 
         {/* Row-14 Settings */}
-        <div className="flex flex-row mb-2 w-full items-center justify-between">
+        <div className="flex flex-row mb-1 w-full items-center justify-between">
           <div className="flex w-full p-1 rounded-md items-center flex-row">
             <svg
               viewBox="0 0 24 24"
@@ -279,19 +279,41 @@ const Sidebar = () => {
         </div>
 
         {/* Row-14 Help */}
-        <div className="flex flex-row mb-2 w-full items-center justify-between">
-          <div className="flex w-full p-1 rounded-md items-center flex-row">
+        <div className="flex flex-row pb-6 border-b w-full items-center justify-between">
+          <div className="flex w-full text-gray-600 p-1 rounded-md items-center flex-row">
             <svg
-              viewBox="0 0 24 24"
-              className=" fill-gray-600"
+              viewBox="0 0 512 512"
+              className="fill-gray-600"
               height="1.5em"
               width="1.5em"
             >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M3.34 17a10.018 10.018 0 01-.978-2.326 3 3 0 00.002-5.347A9.99 9.99 0 014.865 4.99a3 3 0 004.631-2.674 9.99 9.99 0 015.007.002 3 3 0 004.632 2.672A9.99 9.99 0 0120.66 7c.433.749.757 1.53.978 2.326a3 3 0 00-.002 5.347 9.99 9.99 0 01-2.501 4.337 3 3 0 00-4.631 2.674 9.99 9.99 0 01-5.007-.002 3 3 0 00-4.632-2.672A10.018 10.018 0 013.34 17zm5.66.196a4.993 4.993 0 012.25 2.77c.499.047 1 .048 1.499.001A4.993 4.993 0 0115 17.197a4.993 4.993 0 013.525-.565c.29-.408.54-.843.748-1.298A4.993 4.993 0 0118 12c0-1.26.47-2.437 1.273-3.334a8.126 8.126 0 00-.75-1.298A4.993 4.993 0 0115 6.804a4.993 4.993 0 01-2.25-2.77c-.499-.047-1-.048-1.499-.001A4.993 4.993 0 019 6.803a4.993 4.993 0 01-3.525.565 7.99 7.99 0 00-.748 1.298A4.993 4.993 0 016 12a4.99 4.99 0 01-1.273 3.334 8.126 8.126 0 00.75 1.298A4.993 4.993 0 019 17.196zM12 15a3 3 0 110-6 3 3 0 010 6zm0-2a1 1 0 100-2 1 1 0 000 2z" />
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M464 256 A208 208 0 0 1 256 464 A208 208 0 0 1 48 256 A208 208 0 0 1 464 256 z"
+              />
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M336 256 A80 80 0 0 1 256 336 A80 80 0 0 1 176 256 A80 80 0 0 1 336 256 z"
+              />
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M208 54l8 132M296 186l8-132M208 458l8-132M296 326l8 132M458 208l-132 8M326 296l132 8M54 208l132 8M186 296l-132 8"
+              />
             </svg>
             <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
-              Settings
+              Help
             </span>
           </div>
         </div>
