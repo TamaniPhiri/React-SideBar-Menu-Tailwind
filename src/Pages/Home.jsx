81b747d946858d4ed6187e7764e2e9876f2b67c9
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 16:48:10 +0200

    folder structure

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,12 @@
-import Hero from "../Components/Hero"
-import Sidebar from "../Components/Sidebar"
+import Hero from "../Components/Hero";
+import Sidebar from "../Components/Sidebar";
 const Home = () => {
   return (
     <div className="flex w-full flex-row items-center">
-        <Sidebar/>
-        <Hero/>
+      <Sidebar />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
