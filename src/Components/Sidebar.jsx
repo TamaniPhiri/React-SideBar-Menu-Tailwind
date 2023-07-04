1a55546dcc417b14aa714fdda1f666adab37c86a
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 18:39:44 +0200

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -170,7 +170,7 @@ const Sidebar = () => {
           <div className="flex w-full p-1 rounded-md items-center flex-row">
             <svg fill="none" viewBox="0 0 15 15" height="1.5em" width="1.5em">
               <path
-                fill="currentColor"
+                className=" fill-gray-600"
                 fillRule="evenodd"
                 d="M8.697.04a.5.5 0 01.296.542L8.09 6h4.41a.5.5 0 01.4.8l-6 8a.5.5 0 01-.893-.382L6.91 9H2.5a.5.5 0 01-.4-.8l6-8a.5.5 0 01.597-.16zM3.5 8h4a.5.5 0 01.493.582L7.33 12.56 11.5 7h-4a.5.5 0 01-.493-.582L7.67 2.44 3.5 8z"
                 clipRule="evenodd"
@@ -218,26 +218,83 @@ const Sidebar = () => {
           </div>
         </div>
 
-         {/* Row-11 Business */}
-         <div className="flex flex-row mb-2 w-full items-center justify-between">
+        {/* Row-11 Business */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
           <div className="flex w-full p-1 rounded-md items-center flex-row">
-            <div className="bg-[#afe9da] h-2 p-2 rounded-md border-green-600 border w-2]"></div>
+            <div className="bg-[#c6d2fc] h-2 p-2 rounded-md border-purple-600 border w-2]"></div>
             <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
               Business
             </span>
           </div>
         </div>
 
-         {/* Row-12 Travel */}
-         <div className="flex flex-row mb-2 w-full items-center justify-between">
+        {/* Row-12 Travel */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
           <div className="flex w-full p-1 rounded-md items-center flex-row">
-            <div className="bg-[#afe9da] h-2 p-2 rounded-md border-green-600 border w-2]"></div>
+            <div className="bg-[#e1c6f2] h-2 p-2 rounded-md border-pink-600 border w-2]"></div>
             <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
               Travel
             </span>
           </div>
         </div>
 
+        {/* Row-13 Add New Project */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
+          <div className="flex w-full p-1 rounded-md gap-1 items-center flex-row">
+            <div className="border border-gray-400 rounded-md border]">
+              <svg
+                className="fill-gray-400"
+                viewBox="0 0 16 16"
+                height="1.3em"
+                width="1.3em"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 2a.5.5 0 01.5.5v5h5a.5.5 0 010 1h-5v5a.5.5 0 01-1 0v-5h-5a.5.5 0 010-1h5v-5A.5.5 0 018 2z"
+                />
+              </svg>
+            </div>
+            <span className="w-full bg-inherit text-gray-500 flex px-">
+              Add New Project
+            </span>
+          </div>
+        </div>
+
+        {/* Row-14 Settings */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
+          <div className="flex w-full p-1 rounded-md items-center flex-row">
+            <svg
+              viewBox="0 0 24 24"
+              className=" fill-gray-600"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M3.34 17a10.018 10.018 0 01-.978-2.326 3 3 0 00.002-5.347A9.99 9.99 0 014.865 4.99a3 3 0 004.631-2.674 9.99 9.99 0 015.007.002 3 3 0 004.632 2.672A9.99 9.99 0 0120.66 7c.433.749.757 1.53.978 2.326a3 3 0 00-.002 5.347 9.99 9.99 0 01-2.501 4.337 3 3 0 00-4.631 2.674 9.99 9.99 0 01-5.007-.002 3 3 0 00-4.632-2.672A10.018 10.018 0 013.34 17zm5.66.196a4.993 4.993 0 012.25 2.77c.499.047 1 .048 1.499.001A4.993 4.993 0 0115 17.197a4.993 4.993 0 013.525-.565c.29-.408.54-.843.748-1.298A4.993 4.993 0 0118 12c0-1.26.47-2.437 1.273-3.334a8.126 8.126 0 00-.75-1.298A4.993 4.993 0 0115 6.804a4.993 4.993 0 01-2.25-2.77c-.499-.047-1-.048-1.499-.001A4.993 4.993 0 019 6.803a4.993 4.993 0 01-3.525.565 7.99 7.99 0 00-.748 1.298A4.993 4.993 0 016 12a4.99 4.99 0 01-1.273 3.334 8.126 8.126 0 00.75 1.298A4.993 4.993 0 019 17.196zM12 15a3 3 0 110-6 3 3 0 010 6zm0-2a1 1 0 100-2 1 1 0 000 2z" />
+            </svg>
+            <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
+              Settings
+            </span>
+          </div>
+        </div>
+
+        {/* Row-14 Help */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
+          <div className="flex w-full p-1 rounded-md items-center flex-row">
+            <svg
+              viewBox="0 0 24 24"
+              className=" fill-gray-600"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M3.34 17a10.018 10.018 0 01-.978-2.326 3 3 0 00.002-5.347A9.99 9.99 0 014.865 4.99a3 3 0 004.631-2.674 9.99 9.99 0 015.007.002 3 3 0 004.632 2.672A9.99 9.99 0 0120.66 7c.433.749.757 1.53.978 2.326a3 3 0 00-.002 5.347 9.99 9.99 0 01-2.501 4.337 3 3 0 00-4.631 2.674 9.99 9.99 0 01-5.007-.002 3 3 0 00-4.632-2.672A10.018 10.018 0 013.34 17zm5.66.196a4.993 4.993 0 012.25 2.77c.499.047 1 .048 1.499.001A4.993 4.993 0 0115 17.197a4.993 4.993 0 013.525-.565c.29-.408.54-.843.748-1.298A4.993 4.993 0 0118 12c0-1.26.47-2.437 1.273-3.334a8.126 8.126 0 00-.75-1.298A4.993 4.993 0 0115 6.804a4.993 4.993 0 01-2.25-2.77c-.499-.047-1-.048-1.499-.001A4.993 4.993 0 019 6.803a4.993 4.993 0 01-3.525.565 7.99 7.99 0 00-.748 1.298A4.993 4.993 0 016 12a4.99 4.99 0 01-1.273 3.334 8.126 8.126 0 00.75 1.298A4.993 4.993 0 019 17.196zM12 15a3 3 0 110-6 3 3 0 010 6zm0-2a1 1 0 100-2 1 1 0 000 2z" />
+            </svg>
+            <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
+              Settings
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
