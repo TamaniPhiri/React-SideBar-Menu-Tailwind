d1384ef4c81a49bdc7d5d02f608c19e0bd359443
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 17:05:51 +0200

    folder structure

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,27 @@
 const Sidebar = () => {
   return (
     <div className="flex flex-col w-1/3">
-        <div className="flex flex-col w-full">
+        <div className="flex text-sm flex-col w-full">
             <div className="flex flex-row w-full justify-between">
-                <div>
-                    hello
+                <div className="flex items-center flex-row">
+                    <div className="bg-[#4934f5] text-white rounded-md flex">
+                        <span className="p-2">
+                            WL
+                        </span>
+                    </div>
+                    <div>
+                        <h1>widelab</h1>
+                        <span>
+                            Team Plan
+                        </span>
+                    </div>
                 </div>
                 <div>
                     there
                 </div>
+            </div>
+            <div>
+
             </div>
         </div>
     </div>
