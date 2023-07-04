ceca7b4c688d35d4157fca35ee5d56891ce21bf2
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 17:49:26 +0200

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,6 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col w-1/3 p-3 m-1 shadow-md rounded-md">
       <div className="flex text-sm flex-col w-full items-center">
-
         {/* Row-1 wideLab TeamPlan */}
         <div className="flex flex-row w-full items-center justify-between">
           <div className="flex items-center gap-2 flex-row">
@@ -69,6 +68,34 @@ const Sidebar = () => {
                 d="M7.754.82a.5.5 0 00-.508 0l-5.5 3.25a.5.5 0 000 .86l5.5 3.25a.5.5 0 00.508 0l5.5-3.25a.5.5 0 000-.86L7.754.82zM7.5 7.17L2.983 4.5 7.5 1.83l4.517 2.67L7.5 7.17zm-5.93.326a.5.5 0 01.684-.176l5.246 3.1 5.246-3.1a.5.5 0 11.508.86l-5.5 3.25a.5.5 0 01-.508 0l-5.5-3.25a.5.5 0 01-.177-.684zm0 3a.5.5 0 01.684-.177l5.246 3.1 5.246-3.1a.5.5 0 01.508.861l-5.5 3.25a.5.5 0 01-.508 0l-5.5-3.25a.5.5 0 01-.177-.684z"
                 clipRule="evenodd"
               />
+            </svg>
+            <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
+              Inbox
+            </span>
+            <div className="flex text-gray-600 items-center bg-gray-200 rounded-md p-1">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                height="1.5em"
+                width="1.5em"
+              >
+                <path d="M3.5 2A1.5 1.5 0 015 3.5V5H3.5a1.5 1.5 0 110-3zM6 5V3.5A2.5 2.5 0 103.5 6H5v4H3.5A2.5 2.5 0 106 12.5V11h4v1.5a2.5 2.5 0 102.5-2.5H11V6h1.5A2.5 2.5 0 1010 3.5V5H6zm4 1v4H6V6h4zm1-1V3.5A1.5 1.5 0 1112.5 5H11zm0 6h1.5a1.5 1.5 0 11-1.5 1.5V11zm-6 0v1.5A1.5 1.5 0 113.5 11H5z" />
+              </svg>
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Row-4 Activity */}
+        <div className="flex flex-row mb-2 w-full items-center justify-between">
+          <div className="flex w-full p-1 rounded-md items-center flex-row">
+            <svg
+              viewBox="0 0 448 512"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path d="M256 32v17.88C328.5 61.39 384 124.2 384 200v33.4c0 45.4 15.5 89.5 43.8 125l14.9 18.6c5.8 7.2 6.9 17.1 2.9 25.4-4 8.3-12.4 13.6-21.6 13.6H24c-9.23 0-17.635-5.3-21.631-13.6A24.019 24.019 0 015.26 377l14.91-18.6C48.54 322.9 64 278.8 64 233.4V200c0-75.8 55.5-138.61 128-150.12V32c0-17.67 14.3-32 32-32s32 14.33 32 32zm-40 64c-57.4 0-104 46.6-104 104v33.4c0 47.9-13.88 94.6-39.69 134.6H375.7c-25.8-40-39.7-86.7-39.7-134.6V200c0-57.4-46.6-104-104-104h-16zm72 352c0 16.1-6.7 33.3-18.7 45.3S240.1 512 224 512c-17 0-33.3-6.7-45.3-18.7S160 464.1 160 448h128z" />
             </svg>
             <span className="w-full bg-inherit text-gray-800 flex px-2 placeholder-gray-600">
               Inbox
