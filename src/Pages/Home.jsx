56ef55732ac9f011ac3dd51c9f32589fda80261a
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-04 17:52:56 +0200

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../Components/Hero";
 import Sidebar from "../Components/Sidebar";
 const Home = () => {
   return (
-    <div className="flex w-full flex-row items-center">
+    <div className="flex px-4 md:px-8 py-4 w-full flex-row items-center">
       <Sidebar />
       <Hero />
     </div>
